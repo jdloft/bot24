6818e67d639a189bb5a01e66dc6b144bbbd951f0
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-08-03 16:47:09 +0000

    Initial commit

---
+++ bot24.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Bot24 - A bot for performing misc tasks on Wikimedia sites
+# Copyright (C) 2015 Jamison Lofthouse
+#
+# This file is part of Bot24.
+#
+# Bot24 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Bot24 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Bot24.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+import pywikibot
+
+print("Hi")
