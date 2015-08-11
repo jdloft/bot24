23b656cb5fe133c160f5967394b05531f72786a3
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-08-11 17:43:28 +0000

    Create dispatcher and add logging

--- bot24.py
+++ bot24.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 # Bot24 - A bot for performing misc tasks on Wikimedia sites
+# Bot24.py - Main dispatcher for tasks
 # Copyright (C) 2015 Jamison Lofthouse
 #
 # This file is part of Bot24.
@@ -18,7 +19,26 @@
 # You should have received a copy of the GNU General Public License
 # along with Bot24.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import unicode_literals
-import pywikibot
+# Parts of logging and threading taken from legoktm's legobot dispatcher
 
-print("Hi")
+from __future__ import unicode_literals
+
+import os
+import sys
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+# Setup log file
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+log_path = cur_dir + "/logs/dispatcher.log" # path to log
+logger = logging.getLogger('dispatcher') # create logger
+logger.setLevel(logging.DEBUG) # set overall logging level cutoff
+handler = TimedRotatingFileHandler(log_path, when='W0', backupCount=20, utc=True) # create a rotating handler (once a week rotation)
+handler.setLevel(logging.DEBUG) # set handler log level
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')) # log file format
+logger.addHandler(handler)
+out_handler = logging.StreamHandler(sys.stdout) # create stdout handler
+out_handler.setLevel(logging.INFO) # only see INFO messages
+out_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')) # stdout format
+logger.addHandler(out_handler)
+
