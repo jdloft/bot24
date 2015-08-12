ff72f747893c893c32b1c53f42205d214143a456
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-08-12 02:55:37 +0000

    Fix inline comments

--- bot24.py
+++ bot24.py
@@ -30,15 +30,15 @@ from logging.handlers import TimedRotatingFileHandler
 
 # Setup log file
 cur_dir = os.path.dirname(os.path.abspath(__file__))
-log_path = cur_dir + "/logs/dispatcher.log" # path to log
-logger = logging.getLogger('dispatcher') # create logger
-logger.setLevel(logging.DEBUG) # set overall logging level cutoff
-handler = TimedRotatingFileHandler(log_path, when='W0', backupCount=20, utc=True) # create a rotating handler (once a week rotation)
-handler.setLevel(logging.DEBUG) # set handler log level
-handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')) # log file format
+log_path = cur_dir + "/logs/dispatcher.log"  # path to log
+logger = logging.getLogger('dispatcher')  # create logger
+logger.setLevel(logging.DEBUG)  # set overall logging level cutoff
+handler = TimedRotatingFileHandler(log_path, when='W0', backupCount=20, utc=True)  # create a rotating handler (once a week rotation)
+handler.setLevel(logging.DEBUG)  # set handler log level
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))  # log file format
 logger.addHandler(handler)
-out_handler = logging.StreamHandler(sys.stdout) # create stdout handler
-out_handler.setLevel(logging.INFO) # only see INFO messages
-out_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')) # stdout format
+out_handler = logging.StreamHandler(sys.stdout)  # create stdout handler
+out_handler.setLevel(logging.INFO)  # only see INFO messages
+out_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))  # stdout format
 logger.addHandler(out_handler)
 
